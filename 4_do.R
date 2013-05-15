7f50814200bf20b5bb36bca13ab860ba89f515e4
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-15 11:54:29 +0100

    Made graphs prettier and more sensical
    Changed to using a brewer palette for the large graphs
    may later try to make it so that each gene is the same
    colour across graphs...
    
    Changed titles to reflect what they actually show now.

--- 4_do.R
+++ 4_do.R
@@ -34,42 +34,32 @@ tidal_average_filtered <- tidal_average[tidal_average$Probeset.ID %in% tidal_fil
 ##########
 
 
-# Which genes are present (barplots of counts for each gene)
-raw <- BaseBarGraph(perfect_hits)
-raw + ggtitle("Summary of mitochondrial representation on NimbleGen arrays") + 
-    ylab("Count")
-
-
 # line graph of each contig average over biological replicates at each timepoint
-#pdf("mitoexpression%03d.pdf", width = 8.3, height = 11.7, onefile = FALSE)
-
-mito_colour_palette <-  c("chartreuse", "darkmagenta","turquoise", "indianred", "purple", "deepskyblue","firebrick", "gold", "gray86", "moccasin")
-    
 raw_timecourse <- BaseLineGraph(arrays_average) 
-raw_timecourse + scale_color_manual("gene", values = mito_colour_palette) +
-    ggtitle("Mitochondrial gene expression over whole time course")
+raw_timecourse + scale_color_brewer("gene", palette = "Set3") +
+    ggtitle("Mitochondrial gene expression: all time points")
 
 # line graph for each contig: tidal
 tidal <- BaseLineGraph(tidal_average)    
-tidal + scale_color_manual("gene", values = mito_colour_palette) +
+tidal + scale_color_brewer("gene", palette = "Set3") +
     ggtitle("Mitochondrial gene expression: tidal time")
 
 
 # filtered graph: tidal (plus error bars)
 tidal_filtered <- BaseLineGraph(tidal_average_filtered)                          
 tidal_filtered + 
-    ggtitle("Mitochondrial gene expression: tidal filtered for >0.5 expression change") +
+    ggtitle("Mitochondrial gene expression: tidal, significant change (p < 0.05)") +
     geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
 
 # line graph for each contig: circadian
 circa <- BaseLineGraph(circadian_average)
-circa + scale_color_manual("gene", values = mito_colour_palette) + 
+circa + scale_color_brewer("gene", palette = "Set3") + 
     ggtitle("Mitochondrial gene expression: circadian time") 
     
 
 # filtered graph: circadian
 circa_filtered <- BaseLineGraph(circadian_average_filtered)
 circa_filtered + 
-    ggtitle("Mitochondrial gene expression: circadian filtered for >0.5 expression change") + 
+    ggtitle("Mitochondrial gene expression: circadian, significant change (p < 0.05)") + 
     geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
 
