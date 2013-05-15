dc82883705297e8c5d8a62215a938a822ab3af9b
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-15 15:10:01 +0100

    Changed filepaths so that data can be loaded

--- 1_load.R
+++ 1_load.R
@@ -8,9 +8,9 @@ library(gridExtra)
 library(knitr)
 
 # dataframe of array data
-array_data <- read.table("eurydice_arrays/For R/RMA_DATA.txt", header = TRUE)
+array_data <- read.table("RMA_DATA.txt", header = TRUE)
 
 # dataframe of contigs used for the array that have mitochondrial sequence
-mito_contigs <- read.table("Array_Analysis/Using_Ep_blast_v2.2.1/Briancontigs/454_contigs_for_arrays.fastaRESULTS_summary.txt",
+mito_contigs <- read.table("454_contigs_for_arrays.fastaRESULTS_summary.txt",
                            col.names =c("contig_name", "gene", "evalue"))
 
