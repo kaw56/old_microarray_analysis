27011ca40626ff11a48220f6c13013f1ec8045ee
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-17 11:18:16 +0100

    Changed loading to work using probeset data
    I want to use the blast hits to the probes to select
    mitochondrial contigs rather than simply blasting
    the whole contig to try and get a more accurate idea
    of mitochondrial expression by using probes that
    contain mitochondrial sequences.
    
    None of the probes are perfect hits, at some point I
     will need to decide on a cut off point

--- 1_load.R
+++ 1_load.R
@@ -10,7 +10,9 @@ library(xtable)
 # dataframe of array data
 array_data <- read.table("Data/RMA_DATA.txt", header = TRUE)
 
-# dataframe of contigs used for the array that have mitochondrial sequence
-mito_contigs <- read.table("Data/Array_probeset.txtRESULTS_summary.txt",
+# dataframe of Array probesets that match mito genome
+mito_probes <- read.table("Data/array_probeset.txtRESULTS_summary.txt",
                            col.names =c("contig_name", "gene", "evalue"))
 
+# dataframe of key between probes and contigs
+key <- read.table("Data/array_probeset_key.txt", header = TRUE)
