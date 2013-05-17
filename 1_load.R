75417f3a3324153110d4a1741672c72ed629395a
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-17 12:53:33 +0100

    Made mito_probes' header more accurate

--- 1_load.R
+++ 1_load.R
@@ -12,7 +12,7 @@ array_data <- read.table("Data/RMA_DATA.txt", header = TRUE)
 
 # dataframe of Array probesets that match mito genome
 mito_probes <- read.table("Data/array_probeset.txtRESULTS_summary.txt",
-                           col.names =c("contig_name", "gene", "evalue"))
+                           col.names =c("probe", "gene", "evalue"))
 
 # dataframe of key between probes and contigs
 key <- read.table("Data/array_probeset_key.txt", header = TRUE)
