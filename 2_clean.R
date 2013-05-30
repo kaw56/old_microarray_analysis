edb38a00ae896a4ac7dcecb486e048131b963451
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-30 15:25:15 +0100

    duplicated to do forward and reverse analysis.
    There is probably a less stupid way to do this but
    I don't know it/can't be bother to change things now

--- 2_clean.R
+++ 2_clean.R
@@ -8,14 +8,18 @@ library(reshape2)
 perfect_hits <- subset(mito_contigs, evalue == 0)
 
 #drop the rev com duplicates
-perfect_hits <- perfect_hits[!grepl("rc_contig\\w\\w\\w\\w\\w", perfect_hits$contig_name, perl = TRUE),]
-perf_hits_no_rrnL <- subset(perfect_hits, gene != "rrnL")
+perfect_hits_forward <- perfect_hits[!grepl("rc_contig\\w\\w\\w\\w\\w", perfect_hits$contig_name, perl = TRUE),]
+perf_hits_forward_no_rrnL <- subset(perfect_hits_forward, gene != "rrnL")
+
+perfect_hits_reverse<- perfect_hits[grepl("rc_contig\\w\\w\\w\\w\\w", perfect_hits$contig_name, perl = TRUE),]
+perf_hits_reverse_no_rrnL <- subset(perfect_hits_reverse, gene != "rrnL")
 
 # select the perfect hit mitochondrial contigs
-mito_arrays <- array_data[array_data$Probeset.ID %in% perfect_hits$contig_name,]
-
+mito_arrays_forward <- array_data[array_data$Probeset.ID %in% perfect_hits_forward$contig_name,]
+mito_arrays_reverse <- array_data[array_data$Probeset.ID %in% perfect_hits_reverse$contig_name,]
 # add a gene name column
-mito_arrays$gene_name <- perfect_hits[mito_arrays$Probeset.ID %in% perfect_hits$contig_name, "gene"]
+mito_arrays_forward$gene_name <- perfect_hits[mito_arrays$Probeset.ID %in% perfect_hits_forward$contig_name, "gene"]
+mito_arrays_reverse$gene_name <- perfect_hits[mito_arrays$Probeset.ID %in% perfect_hits_reverse$contig_name, "gene"]
 
 without_rrnl <- subset(mito_arrays, gene_name != "rrnL")
 
@@ -25,40 +29,68 @@ without_rrnl <- subset(mito_arrays, gene_name != "rrnL")
 
 
 
-# seperate out time points 
-HW1 <- mito_arrays[, c(1, 2:4, 14)]
-HW1 <- renaming(HW1)
+# seperate out time points
+HW1_forward <- mito_arrays_forward[, c(1, 2:4, 14)]
+HW1_forward <- renaming(HW1_forward)
 
-LW1 <- mito_arrays[, c(1, 5:7, 14)]
-LW1 <-renaming(LW1) 
+HW1_reverse <- mito_arrays_reverse[, c(1, 2:4, 14)]
+HW1_reverse <- renaming(HW1_reverse)
 
-HW2 <- mito_arrays[, c(1, 8:10, 14)]
-HW2 <- renaming(HW2) 
+LW1_forward <- mito_arrays_forward[, c(1, 5:7, 14)]
+LW1_forward <-renaming(LW1_forward)
 
-LW2 <- mito_arrays[, c(1, 11:13, 14)]
-LW2 <- renaming(LW2) 
+LW1_reverse <- mito_arrays_reverse[, c(1, 5:7, 14)]
+LW1_reverse <-renaming(LW1_reverse)
+
+HW2_forward <- mito_arrays_forward[, c(1, 8:10, 14)]
+HW2_forward <- renaming(HW2_forward)
+
+HW2_reverse <- mito_arrays_reverse[, c(1, 8:10, 14)]
+HW2_reverse <- renaming(HW2_reverse)
+
+LW2_forward <- mito_arrays_forward[, c(1, 11:13, 14)]
+LW2_forward <- renaming(LW2_forward)
+
+LW2_reverse <- mito_arrays_reverse[, c(1, 11:13, 14)]
+LW2_reverse <- renaming(LW2_reverse)
 
 # long replicate data
-HW1 <- make_long(HW1)
-LW1 <- make_long(LW1)
-HW2 <- make_long(HW2)
-LW2 <- make_long(LW2)
+HW1_forward <- make_long(HW1_forward)
+LW1_forward <- make_long(LW1_forward)
+HW2_forward <- make_long(HW2_forward)
+LW2_forward <- make_long(LW2_forward)
+
+HW1_reverse <- make_long(HW1_reverse)
+LW1_reverse <- make_long(LW1_reverse)
+HW2_reverse <- make_long(HW2_reverse)
+LW2_reverse <- make_long(LW2_reverse)
 
 # add time column
-HW1$time <- rep("HW1", length(HW1$Probeset.ID)) 
-LW1$time <- rep("LW1", length(HW1$Probeset.ID)) 
-HW2$time <- rep("HW2", length(HW1$Probeset.ID)) 
-LW2$time <- rep("LW2", length(HW1$Probeset.ID)) 
+HW1_forward$time <- rep("HW1", length(HW1_forward$Probeset.ID))
+LW1_forward$time <- rep("LW1", length(HW1_forward$Probeset.ID))
+HW2_forward$time <- rep("HW2", length(HW1_forward$Probeset.ID))
+LW2_forward$time <- rep("LW2", length(HW1_forward$Probeset.ID))
+
+HW1_reverse$time <- rep("HW1", length(HW1_reverse$Probeset.ID))
+LW1_reverse$time <- rep("LW1", length(HW1_reverse$Probeset.ID))
+HW2_reverse$time <- rep("HW2", length(HW1_reverse$Probeset.ID))
+LW2_reverse$time <- rep("LW2", length(HW1_reverse$Probeset.ID))
 
 #combine back to 1 dataframe
-tidal_arrays.long <- rbind(HW1, LW1, HW2, LW2)
-rm(HW1, LW1, HW2, LW2)
+tidal_arrays_forward.long <- rbind(HW1_forward, LW1_forward, HW2_forward, LW2_forward)
+
+tidal_arrays_reverse.long <- rbind(HW1_reverse, LW1_reverse, HW2_reverse, LW2_reverse)
+
+rm(HW1_forward, LW1_forward, HW2_forward, LW2_forward, HW1_reverse, LW1_reverse, HW2_reverse, LW2_reverse)
 
 
 # rearrange columns
-arrays_long <- tidal_arrays.long[c("Probeset.ID", "gene", "time", "replicate", "expression")]
+arrays_long_forward <- tidal_arrays_forward.long[c("Probeset.ID", "gene", "time", "replicate", "expression")]
+arrays_long_reverse <- tidal_arrays_reverse.long[c("Probeset.ID", "gene", "time", "replicate", "expression")]
 
 # make time a factor
-arrays_long$time <- factor(tidal_arrays.long$time, 
+arrays_long_forward$time <- factor(tidal_arrays_forward.long$time,
+                           levels = c("HW1", "LW2", "HW2", "LW1"))
+arrays_long_reverse$time <- factor(tidal_arrays_reverse.long$time,
                            levels = c("HW1", "LW2", "HW2", "LW1"))
 
--- 4_do.R
+++ 4_do.R
@@ -2,109 +2,221 @@
 # summarising and filtering #
 #############################
 
-## @knitr filter
+## @knitr filter_forward
 
 # collapse levels to give tidal and circdian data
-tidal <- arrays_long
-levels(tidal$time) <- c("HW", "LW", "HW", "LW")
+tidal_forward <- arrays_long_forward
+levels(tidal_forward$time) <- c("HW", "LW", "HW", "LW")
 
-circadian <- arrays_long
-levels(circadian$time) <- c("day", "day", "night", "night")
+circadian_forward <- arrays_long_forward
+levels(circadian_forward$time) <- c("day", "day", "night", "night")
 
-# average of the biological replicates at each time point 
+# average of the biological replicates at each time point
 # (plus standard deviation and standard error)
-arrays_average <- summary_stats(arrays_long)
-circadian_average <- summary_stats(circadian)
-tidal_average <- summary_stats(tidal)
+arrays_average_forward <- summary_stats(arrays_long_forward)
+circadian_average_forward <- summary_stats(circadian_forward)
+tidal_average_forward <- summary_stats(tidal_forward)
 
 # t-test to find significant changes
-circadian_t_test <- perform_t_test(circadian)
-tidal_t_test <- perform_t_test(tidal)
+circadian_t_test_forward <- perform_t_test(circadian_forward)
+tidal_t_test_forward <- perform_t_test(tidal_forward)
 
 # filter out unchanging circadian
-circadian_filter_set <-circadian_t_test[circadian_t_test$pvalue < 0.05 ,]
-circadian_average_filtered <- circadian_average[circadian_average$Probeset.ID %in% circadian_filter_set$Probeset.ID,]
+circadian_filter_set_forward <-circadian_t_test_forward[circadian_t_test_forward$pvalue < 0.05 ,]
+circadian_average_filtered_forward <- circadian_average_forward[circadian_average_forward$Probeset.ID %in% circadian_filter_set_forward$Probeset.ID,]
 
 # filter out unchanging sequences tidal
-tidal_filter_set <- tidal_t_test[ tidal_t_test$pvalue < 0.05 ,]
-tidal_average_filtered <- tidal_average[tidal_average$Probeset.ID %in% tidal_filter_set$Probeset.ID,]
+tidal_filter_set_forward <- tidal_t_test_forward[ tidal_t_test_forward$pvalue < 0.05 ,]
+tidal_average_filtered_forward <- tidal_average_forward[tidal_average_forward$Probeset.ID %in% tidal_filter_set_forward$Probeset.ID,]
 
 # number of rrnL hits
-rrnl_num <- nrow(perfect_hits) - nrow(perf_hits_no_rrnL)
+rrnl_num_forward <- nrow(perfect_hits_forward) - nrow(perf_hits_forward_no_rrnL)
 
 ##########
 # tables #
 ##########
 
-## @knitr contig_table
-perf_table <- perf_hits_no_rrnL
+## @knitr contig_table_forward
+perf_table_forward <- perf_hits_forward_no_rrnL
 # drop the evalues
-perf_table$evalue <- NULL
+perf_table_forward$evalue <- NULL
 # reorder columns
-perf_table <- perf_table[c("gene", "contig_name")]
+perf_table_forward <- perf_table_forward[c("gene", "contig_name")]
 # sort by gene
-perf_table <- perf_table[order(perf_table$gene),]
+perf_table_forward <- perf_table_forward[order(perf_table_forward$gene),]
 
-perf_table_print <- xtable(perf_table,
+perf_table_print_forward <- xtable(perf_table_forward,
                             caption = "List of represented mitochondrial genes and their associated contigs")
 
-print.xtable(perf_table_print, include.rownames = FALSE)
+print.xtable(perf_table_print_forward, include.rownames = FALSE)
 
-## @knitr circa_t_test
-circa_t_table <- circadian_t_test
-circa_t_table <- make_t_table(circa_t_table)
+## @knitr circa_t_test_forward
+circa_t_table_forward <- circadian_t_test_forward
+circa_t_table_forward <- make_t_table(circa_t_table_forward)
 
-circa_t_table_print <- xtable(circa_t_table,
+circa_t_table_print_forward <- xtable(circa_t_table_forward,
                               caption = "P values of t-test comparing day time and night time mitochondrial gene expression")
 
-print.xtable(circa_t_table_print, include.rownames = FALSE) 
+print.xtable(circa_t_table_print_forward, include.rownames = FALSE)
 
 
-## @knitr tidal_t_test
-tidal_t_table <- tidal_t_test
-tidal_t_table <- make_t_table(tidal_t_table)
+## @knitr tidal_t_test_forward
+tidal_t_table_forward <- tidal_t_test_forward
+tidal_t_table_forward <- make_t_table(tidal_t_table_forward)
 
-tidal_t_table_print <- xtable(circa_t_table,
+tidal_t_table_print_forward <- xtable(tidal_t_table_forward,
                               caption = "P values of t-test comparing high tide and low tide mitochondrial gene expression")
 
-print.xtable(tidal_t_table_print, include.rownames = FALSE)
+print.xtable(tidal_t_table_print_forward, include.rownames = FALSE)
 
 ##########
 # graphs #
 ##########
 
-## @knitr whole_time_course
+## @knitr whole_time_course_forward
 
 # line graph of each contig average over biological replicates at each timepoint
-raw_timecourse <- BaseLineGraph(arrays_average) 
-raw_timecourse + scale_color_brewer("gene", palette = "Set3") 
+raw_timecourse_forward <- BaseLineGraph(arrays_average_forward)
+raw_timecourse_forward + scale_color_brewer("gene", palette = "Set3")
 
-## @knitr tidal_contig_graph
+## @knitr tidal_contig_graph_forward
 
 # line graph for each contig: tidal
-tidal <- BaseLineGraph(tidal_average)    
-tidal + scale_color_brewer("gene", palette = "Set3") 
+tidal_forward <- BaseLineGraph(tidal_average_forward)
+tidal_forward + scale_color_brewer("gene", palette = "Set3")
 
 
-## @knitr tidal_filter_graph
+## @knitr tidal_filter_graph_forward
 
 # filtered graph: tidal (plus error bars)
-tidal_filtered <- BaseLineGraph(tidal_average_filtered)                          
-tidal_filtered +
+tidal_filtered_forward <- BaseLineGraph(tidal_average_filtered_forward)
+tidal_filtered_forward +
     geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
 
 
-## @knitr circadian_contig_graph
+## @knitr circadian_contig_graph_forward
 
 # line graph for each contig: circadian
-circa <- BaseLineGraph(circadian_average)
-circa + scale_color_brewer("gene", palette = "Set3")  
-    
-## @knitr circa_filter_graph
+circa_forward <- BaseLineGraph(circadian_average_forward)
+circa_forward + scale_color_brewer("gene", palette = "Set3")
+
+## @knitr circa_filter_graph_forward
 
 # filtered graph: circadian
-circa_filtered <- BaseLineGraph(circadian_average_filtered)
-circa_filtered + 
+circa_filtered_forward <- BaseLineGraph(circadian_average_filtered_forward)
+circa_filtered_forward +
+    geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
+
+## @knitr
+
+
+#############################
+# summarising and filtering #
+#############################
+
+## @knitr filter_reverse
+
+# collapse levels to give tidal and circdian data
+tidal_reverse <- arrays_long_reverse
+levels(tidal_reverse$time) <- c("HW", "LW", "HW", "LW")
+
+circadian_reverse <- arrays_long_reverse
+levels(circadian_reverse$time) <- c("day", "day", "night", "night")
+
+# average of the biological replicates at each time point
+# (plus standard deviation and standard error)
+arrays_average_reverse <- summary_stats(arrays_long_reverse)
+circadian_average_reverse <- summary_stats(circadian_reverse)
+tidal_average_reverse <- summary_stats(tidal_reverse)
+
+# t-test to find significant changes
+circadian_t_test_reverse <- perform_t_test(circadian_reverse)
+tidal_t_test_reverse <- perform_t_test(tidal_reverse)
+
+# filter out unchanging circadian
+circadian_filter_set_reverse <-circadian_t_test_reverse[circadian_t_test_reverse$pvalue < 0.05 ,]
+circadian_average_filtered_reverse <- circadian_average_reverse[circadian_average_reverse$Probeset.ID %in% circadian_filter_set_reverse$Probeset.ID,]
+
+# filter out unchanging sequences tidal
+tidal_filter_set_reverse <- tidal_t_test_reverse[ tidal_t_test_reverse$pvalue < 0.05 ,]
+tidal_average_filtered_reverse <- tidal_average_reverse[tidal_average_reverse$Probeset.ID %in% tidal_filter_set_reverse$Probeset.ID,]
+
+# number of rrnL hits
+rrnl_num_reverse <- nrow(perfect_hits_reverse) - nrow(perf_hits_reverse_no_rrnL)
+
+##########
+# tables #
+##########
+
+## @knitr contig_table_reverse
+perf_table_reverse <- perf_hits_reverse_no_rrnL
+# drop the evalues
+perf_table_reverse$evalue <- NULL
+# reorder columns
+perf_table_reverse<- perf_table_reverse[c("gene", "contig_name")]
+# sort by gene
+perf_table_reverse <- perf_table_reverse[order(perf_table_reverse$gene),]
+
+perf_table_print_reverse <- xtable(perf_table_reverse,
+                           caption = "List of represented mitochondrial genes and their associated contigs")
+
+print.xtable(perf_table_print_reverse, include.rownames = FALSE)
+
+## @knitr circa_t_test_reverse
+circa_t_table_reverse <- circadian_t_test_reverse
+circa_t_table_reverse <- make_t_table(circa_t_table_reverse)
+
+circa_t_table_print_reverse <- xtable(circa_t_table_reverse,
+                              caption = "P values of t-test comparing day time and night time mitochondrial gene expression")
+
+print.xtable(circa_t_table_print_reverse, include.rownames = FALSE)
+
+
+## @knitr tidal_t_test
+tidal_t_table_reverse <- tidal_t_test_reverse
+tidal_t_table_reverse <- make_t_table(tidal_t_table_reverse)
+
+tidal_t_table_print_reverse <- xtable(tidal_t_table_reverse,
+                              caption = "P values of t-test comparing high tide and low tide mitochondrial gene expression")
+
+print.xtable(tidal_t_table_print_reverse, include.rownames = FALSE)
+
+##########
+# graphs #
+##########
+
+## @knitr whole_time_course_reverse
+
+# line graph of each contig average over biological replicates at each timepoint
+raw_timecourse_reverse <- BaseLineGraph(arrays_average_reverse)
+raw_timecourse_reverse + scale_color_brewer("gene", palette = "Set3")
+
+## @knitr tidal_contig_graph_reverse
+
+# line graph for each contig: tidal
+tidal_reverse <- BaseLineGraph(tidal_average_reverse)
+tidal_reverse + scale_color_brewer("gene", palette = "Set3")
+
+
+## @knitr tidal_filter_graph_reverse
+
+# filtered graph: tidal (plus error bars)
+tidal_filtered_reverse <- BaseLineGraph(tidal_average_filtered_reverse)
+tidal_filtered_reverse +
+    geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
+
+
+## @knitr circadian_contig_graph_reverse
+
+# line graph for each contig: circadian
+circa_reverse <- BaseLineGraph(circadian_average_reverse)
+circa_reverse + scale_color_brewer("gene", palette = "Set3")
+
+## @knitr circa_filter_graph_reverse
+
+# filtered graph: circadian
+circa_filtered_reverse<- BaseLineGraph(circadian_average_filtered_reverse)
+circa_filtered_reverse +
     geom_errorbar(aes(ymin=mean_expression-se, ymax=mean_expression+se), width=.1)
 
 ## @knitr
