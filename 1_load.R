21ac20f7171e7c593b00d34eb81873d9cad6e1d9
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-06-04 13:30:55 +0100

    Realised I need the key to access the RMA data
    I'm still using Brian's RMA data (because it's
    quicker to get to an interesting point) so I need
    the key again.
    
    I really should merge this into the
    array_probeset_analysis part because this is an
    advance on that really...

--- 1_load.R
+++ 1_load.R
@@ -10,6 +10,9 @@ library(xtable)
 # dataframe of array data
 array_data <- read.table("Data/RMA_DATA.txt", header = TRUE)
 
+# dataframe of key between probes and contigs
+key <- read.table("Data/array_probeset_key.txt", header = TRUE)
+
 # dataframe of contigs used for the array that have mitochondrial sequence
 mito_contigs <- read.table("Data/Array_probeset.txtRESULTS_summary.txt",
                            col.names =c("probe", "gene", "score", "length", "start", "end"))
